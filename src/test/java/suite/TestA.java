47c967ae7bd9264bc1b668bc23ea7f378f559ea5
Pan Guo Feng <panguof@gmail.com>
2016-03-06 21:58:30 +0900

    junit - test suite

--- src/test/java/suite/TestA.java
+++ src/test/java/suite/TestA.java
@@ -1,7 +1,15 @@
 package suite;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by pan on 2016/3/6.
  */
 public class TestA {
+
+    @Test
+    public void aTest(){
+        Assert.assertTrue(true);
+    }
 }
--- src/test/java/suite/TestB.java
+++ src/test/java/suite/TestB.java
@@ -1,7 +1,15 @@
 package suite;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by pan on 2016/3/6.
  */
 public class TestB {
+
+    @Test
+    public void bTest(){
+        Assert.assertTrue(true);
+    }
 }
